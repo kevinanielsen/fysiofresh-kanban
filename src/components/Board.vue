3b08474415d75f0134a62c6dc0bdaf12251850c1
Kevin Nielsen <kevinanielsen@outlook.com>
2024-01-05 16:59:27 +0100

    feat: add task CreateTask feature

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -2,47 +2,60 @@
 import { ref } from "vue";
 import draggable from "vuedraggable";
 import Task from "./Task.vue";
+import CreateTask from "./CreateTask.vue";
 
 const data = ref({
   todo: [
     {
       title: "Task",
-      description: "",
+      description: "This is a short task",
     },
   ],
   doing: [],
   done: [],
 });
+
+const addTask = (
+  e: SubmitEvent,
+  task: { title: string; description: string }
+) => {
+  e.preventDefault();
+  data.value.todo.push(task);
+};
+
 const h3Styles = {
   class: "bg-[#603F3E] text-white p-2 rounded-[4px] mb-2 font-bold text-lg",
 };
 </script>
 
 <template>
-  <div class="flex gap-4 w-full">
-    <div id="todo" class="w-1/3 border shadow-inner rounded-[24px] p-2">
-      <h3 v-bind="h3Styles">To Do</h3>
-      <draggable :list="data.todo" itemKey="title" group="tasks">
-        <template #item="{ element }">
-          <Task :title="element.title" :description="element.description" />
-        </template>
-      </draggable>
-    </div>
-    <div id="doing" class="w-1/3 border shadow-inner rounded-[24px] p-2">
-      <h3 v-bind="h3Styles">Doing</h3>
-      <draggable :list="data.doing" itemKey="title" group="tasks">
-        <template #item="{ element }">
-          <Task :title="element.title" :description="element.description" />
-        </template>
-      </draggable>
-    </div>
-    <div id="done" class="w-1/3 border shadow-inner rounded-[24px] p-2">
-      <h3 v-bind="h3Styles">Done</h3>
-      <draggable :list="data.done" itemKey="title" group="tasks">
-        <template #item="{ element }">
-          <Task :title="element.title" :description="element.description" />
-        </template>
-      </draggable>
+  <div class="flex flex-col w-full gap-4">
+    <CreateTask :addTask="addTask" />
+    <div class="flex gap-4 w-full">
+      <div id="todo" class="w-1/3 border shadow-inner rounded-lg p-2">
+        <h3 v-bind="h3Styles">To Do</h3>
+        <draggable :list="data.todo" itemKey="title" group="tasks">
+          <template #item="{ element }">
+            <Task :title="element.title" :description="element.description" />
+          </template>
+        </draggable>
+      </div>
+      <div id="doing" class="w-1/3 border shadow-inner rounded-lg p-2">
+        <h3 v-bind="h3Styles">Doing</h3>
+        <draggable :list="data.doing" itemKey="title" group="tasks">
+          <template #item="{ element }">
+            <Task :title="element.title" :description="element.description" />
+          </template>
+        </draggable>
+      </div>
+      <div id="done" class="w-1/3 border shadow-inner rounded-lg p-2">
+        <h3 v-bind="h3Styles">Done</h3>
+        <draggable :list="data.done" itemKey="title" group="tasks">
+          <template #item="{ element }">
+            <Task :title="element.title" :description="element.description" />
+          </template>
+        </draggable>
+      </div>
     </div>
   </div>
 </template>
---
+++ src/components/CreateTask.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+defineProps({
+  addTask: Function,
+});
+
+const title = ref("");
+const description = ref("");
+
+const labelClass = "flex flex-col w-1/3 h-full";
+</script>
+
+<template>
+  <form
+    class="bg-[#FFECBE] w-full flex p-4 gap-4 rounded-xl"
+    @submit="(event) => addTask && addTask(event, { title, description })"
+  >
+    <label for="title" v-bind:class="labelClass">
+      Title
+      <input
+        type="text"
+        name="title"
+        id="title"
+        class="bg-black/10 rounded-[4px]"
+        aria-label="Title"
+        v-model="title"
+      />
+    </label>
+    <label for="description" v-bind:class="labelClass">
+      Description
+      <input
+        type="text"
+        name="description"
+        id="description"
+        class="bg-black/10 rounded-[4px]"
+        aria-label="description"
+        v-model="description"
+      />
+    </label>
+    <button class="bg-[#53302F] shadow-lg rounded-[4px] px-4 text-white w-1/3">
+      Add task
+    </button>
+  </form>
+</template>
