e3f82f4a6f9f95b7c4d0a26345f6b9368aa1cf87
Kevin Nielsen <kevinanielsen@outlook.com>
2024-01-05 12:03:51 +0100

    feat: setup vuedraggable

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,8 @@
-<script setup lang="ts">
-import draggable from "vuedraggable";
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <div class="w-full h-full flex flex-col items-center p-4">
     <h1 class="text-3xl">Fysiofresh Kanban Board</h1>
-    <draggable> </draggable>
+    <Board />
   </div>
 </template>
---
+++ src/components/Board.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import draggable from "vuedraggable";
+
+const data = ref({ todo: [], doing: [], done: [] });
+</script>
+
+<template>
+  <div class="row">
+    <div id="todo">
+      <h3>To Do</h3>
+      <draggable :list="data.todo" itemKey="todo">
+        <template #item="{ element }">
+          <div class="">{{ element.name }}</div>
+        </template>
+      </draggable>
+    </div>
+    <div id="doing">
+      <h3>Doing</h3>
+      <draggable :list="data.doing" itemKey="doing">
+        <template #item="{ element }">
+          <div class="">{{ element.name }}</div>
+        </template>
+      </draggable>
+    </div>
+    <div id="done">
+      <h3>Done</h3>
+      <draggable :list="data.done" itemKey="done">
+        <template #item="{ element }">
+          <div class="">{{ element.name }}</div>
+        </template>
+      </draggable>
+    </div>
+  </div>
+</template>
