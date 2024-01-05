c2a02c57dccdb246a7abf46156aef93f10690bfe
Kevin Nielsen <kevinanielsen@outlook.com>
2024-01-05 15:16:47 +0100

    chore: add h3Styles var

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -12,12 +12,15 @@ const data = ref({
   doing: [],
   done: [],
 });
+const h3Styles = {
+  class: "bg-[#603F3E] text-white p-2 rounded-[16px] mb-2 font-bold text-lg",
+};
 </script>
 
 <template>
   <div class="flex gap-4 w-full">
     <div id="todo" class="w-1/3 border shadow-inner rounded-[24px] p-2">
-      <h3 class="bg-[#603F3E] text-white p-2 rounded-[16px] mb-2">To Do</h3>
+      <h3 v-bind="h3Styles">To Do</h3>
       <draggable :list="data.todo" itemKey="title" group="tasks">
         <template #item="{ element }">
           <div class="">{{ element.title }}</div>
@@ -25,7 +28,7 @@ const data = ref({
       </draggable>
     </div>
     <div id="doing" class="w-1/3 border shadow-inner rounded-[24px] p-2">
-      <h3 class="bg-[#603F3E] text-white p-2 rounded-[16px] mb-2">Doing</h3>
+      <h3 v-bind="h3Styles">Doing</h3>
       <draggable :list="data.doing" itemKey="title" group="tasks">
         <template #item="{ element }">
           <div class="">{{ element.title }}</div>
@@ -33,7 +36,7 @@ const data = ref({
       </draggable>
     </div>
     <div id="done" class="w-1/3 border shadow-inner rounded-[24px] p-2">
-      <h3 class="bg-[#603F3E] text-white p-2 rounded-[16px] mb-2">Done</h3>
+      <h3 v-bind="h3Styles">Done</h3>
       <draggable :list="data.done" itemKey="title" group="tasks">
         <template #item="{ element }">
           <div class="">{{ element.title }}</div>
