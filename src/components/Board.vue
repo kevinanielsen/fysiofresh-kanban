b40db65ce318cbb8239ad57e9edcccf3cc964cea
Kevin Nielsen <kevinanielsen@outlook.com>
2024-01-05 12:38:50 +0100

    style: add styles for the lists

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -2,32 +2,41 @@
 import { ref } from "vue";
 import draggable from "vuedraggable";
 
-const data = ref({ todo: [], doing: [], done: [] });
+const data = ref({
+  todo: [
+    {
+      title: "Task",
+      description: "",
+    },
+  ],
+  doing: [],
+  done: [],
+});
 </script>
 
 <template>
-  <div class="row">
-    <div id="todo">
-      <h3>To Do</h3>
-      <draggable :list="data.todo" itemKey="todo">
+  <div class="flex gap-4 w-full">
+    <div id="todo" class="w-1/3 border shadow-inner rounded-[24px] p-2">
+      <h3 class="bg-[#603F3E] text-white p-2 rounded-[16px] mb-2">To Do</h3>
+      <draggable :list="data.todo" itemKey="title" group="tasks">
         <template #item="{ element }">
-          <div class="">{{ element.name }}</div>
+          <div class="">{{ element.title }}</div>
         </template>
       </draggable>
     </div>
-    <div id="doing">
-      <h3>Doing</h3>
-      <draggable :list="data.doing" itemKey="doing">
+    <div id="doing" class="w-1/3 border shadow-inner rounded-[24px] p-2">
+      <h3 class="bg-[#603F3E] text-white p-2 rounded-[16px] mb-2">Doing</h3>
+      <draggable :list="data.doing" itemKey="title" group="tasks">
         <template #item="{ element }">
-          <div class="">{{ element.name }}</div>
+          <div class="">{{ element.title }}</div>
         </template>
       </draggable>
     </div>
-    <div id="done">
-      <h3>Done</h3>
-      <draggable :list="data.done" itemKey="done">
+    <div id="done" class="w-1/3 border shadow-inner rounded-[24px] p-2">
+      <h3 class="bg-[#603F3E] text-white p-2 rounded-[16px] mb-2">Done</h3>
+      <draggable :list="data.done" itemKey="title" group="tasks">
         <template #item="{ element }">
-          <div class="">{{ element.name }}</div>
+          <div class="">{{ element.title }}</div>
         </template>
       </draggable>
     </div>
