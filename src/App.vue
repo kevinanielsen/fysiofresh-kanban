d6efcf8bfa2ce92d7af88230fc75f37ca72aa3aa
Kevin Nielsen <kevinanielsen@outlook.com>
2024-01-05 11:42:07 +0100

    chore: add vuedraggable

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,10 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import draggable from "vuedraggable";
+</script>
 
-<template></template>
+<template>
+  <div class="w-full h-full flex flex-col items-center p-4">
+    <h1 class="text-3xl">Fysiofresh Kanban Board</h1>
+    <draggable> </draggable>
+  </div>
+</template>
