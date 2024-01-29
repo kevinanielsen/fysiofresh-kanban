a65e73f85454408fb3086a4ad418b338220dac64
Kevin Nielsen <kevinanielsen@outlook.com>
2024-01-29 16:25:44 +0100

    test: addTask action

--- src/stores/__tests__/TasksStore.spec.ts
+++ src/stores/__tests__/TasksStore.spec.ts
@@ -1,4 +1,4 @@
-import { it, expect, beforeEach } from "vitest";
+import { it, expect, beforeEach, describe } from "vitest";
 import { useTasksStore } from "../TasksStore";
 import { createPinia, setActivePinia } from "pinia";
 
@@ -6,18 +6,34 @@ beforeEach(() => {
   setActivePinia(createPinia());
 })
 
-it('should ensure default state', () => {
-  //Given a new store without any changes
-  //When store is created
-  //Then the default state is
+describe("tasksStore", () => {
+  it('should ensure default state', () => {
+    //Given a new store without any changes
+    //When store is created
+    //Then the default state is
 
-  //Arrange
-  const tasksStore = useTasksStore();
+    //Arrange
+    const tasksStore = useTasksStore();
 
-  //Act - we're testing the constructor, there is no act.
+    //Act - we're testing the constructor, there is no act.
 
-  //Assert
-  expect(tasksStore.todo).toMatchSnapshot();
-  expect(tasksStore.doing).toMatchSnapshot();
-  expect(tasksStore.done).toMatchSnapshot();
-});
+    //Assert
+    expect(tasksStore.todo).toMatchSnapshot();
+    expect(tasksStore.doing).toMatchSnapshot();
+    expect(tasksStore.done).toMatchSnapshot();
+  });
+  it("should add a task", () => {
+    //Given a new store without any changes
+    //When a task is added
+    //Then the task is added to the todo list
+
+    //Arrange
+    const tasksStore = useTasksStore();
+
+    //Act
+    tasksStore.addTask("Do the dishes", "Clean the dishes");
+
+    //Assert
+    expect(tasksStore.todo).toMatchSnapshot();
+  });
+})
